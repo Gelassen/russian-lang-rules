c2a374a8bc9bf9f1b545a718a29a4c1fd22f1792
Dmitry Kazakov <dmitriy.kazakov.da@gmail.com>
2020-08-14 15:02:37 +0300

    Add new rules

--- app/src/main/java/ru/home/russianlang/content/Utils.java
+++ app/src/main/java/ru/home/russianlang/content/Utils.java
@@ -35,7 +35,76 @@ public class Utils {
 
         root.setTitle("Безударные окончания слов признаков \n Которые отвечают на вопросы Какой? Какая? Какое? Какие?");
         root.setValue("Пишем такие же окончания, но в той форме, которой требует слово-предмет -- носитель данного признака (в каком? в лёгком блестящем костюме)");
+        root.setExceptions("В мужском род при 'какой?' -ЫЙ, -ИЙ, шар -какой? -синий, -белый");
+        root.setExceptions("В словах на -ИЙ, -ЬЯ, -ЬЕ (принадлежность!) Ь знак во всех формах кроме именительного и винительного падежей мужского рода (лисий, лисьего, лисьему, лисий, лисьим, о лисьем)");
+        return root;
+    }
 
+    public static Node generateRule20_3() {
+        Node root = new Node(null);
+
+        root.setTitle("Гласные в безударных окончаниях слов действий (глаголы)");
+        root.setValue("Из двенадцати глаголов исключений?");
+        root.setPositive(new Node(null, "И//А(Я) (держит, держат)"));
+        root.setNegative(new Node(null, "на -ИТЬ"));
+
+        Node negativeLevel1 = root.getNegative();
+        negativeLevel1.setPositive(new Node(null, "И//А(Я) (строит, строят)"));
+        negativeLevel1.setNegative(new Node(null, "Е//У(Ю) (пишет, пишут)"));
+
+        Node positiveLevel2 = negativeLevel1.getPositive();
+        positiveLevel2.setExceptions("брить, стелить (стелет, стелют)");
+
+        root.setExceptions(
+                "1. Глаголы, у которых личное ударное окончание, с прибавлением приставки 'вы' пишется также как и без неё (бежишь => выбежишь) \n" +
+                "2. Те же и так же пишутся гласные в суффиксах ущ(ющ)//ащ(ящ), ем//им слов признаков, образованных от глагола: строящийся, читаемый");
+        return root;
+    }
+
+    // Правописание глаголов и отглагольных слов
+    public static Node generateRule22_1() {
+        Node root = new Node(null);
+
+        root.setTitle("ИТЕ//ЕТЕ на конце глаголов");
+        root.setValue("повелительная форма?");
+        root.setPositive(new Node(null, "ИТЕ (почистите)"));
+        root.setNegative(new Node(null, "из 11 исключений // на ИТЬ?"));
+
+        Node negativeLevel1 = root.getNegative();
+        negativeLevel1.setPositive(new Node(null, "ИТЕ (строите)"));
+        negativeLevel1.setNegative(new Node(null, "ЕТЕ (пишете)"));
+        return root;
+    }
+
+    public static Node generateRule22_2() {
+        Node root = new Node(null);
+
+        root.setTitle("ТСЯ//ТЬСЯ на конце глаголов");
+        root.setValue("Отвечает на вопрос 'что делает?' (кружится), отвечает на вопрос 'что делать?' (кружиться)");
+
+        return root;
+    }
+
+    public static Node generateRule22_3() {
+        Node root = new Node(null);
+
+        root.setTitle("Гласная перед суффиксами Л//ВШ//В(вши)//ВА//ТЕЛЬ//ТЕЛЬН");
+        root.setValue(
+                "Та же гласная, что перед ТЬ в начальной форме (строить => строил, " +
+                "измазать => измазавший, высмотреть => высмотрев, одолеть => одолевать, " +
+                "множить => множитель, трогать => трогательный)"
+        );
+        root.setExceptions("застрять, затмить, продлить, вать");
+
+        return root;
+    }
+
+    public static Node generateRule22_4() {
+        Node root = new Node(null);
+
+        root.setTitle("НЕ с глаголами");
+        root.setValue("НЕ с глаголами пишется отдельно (не знает, не видет, не слушает)");
+        root.setExceptions("Есть глаголы, которые без НЕ не бывают (негодовать)");
         return root;
     }
 }
